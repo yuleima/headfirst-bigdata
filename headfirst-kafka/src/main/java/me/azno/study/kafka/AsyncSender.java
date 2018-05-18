339f864fede6fd84018ad051abcf030ef58e00ab
yuleima <mayl@zetyun.com>
2018-05-18 18:16:29 +0800

    add kafka module too.

---
+++ headfirst-kafka/src/main/java/me/azno/study/kafka/AsyncSender.java
@@ -0,0 +1,39 @@
+package me.azno.study.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+public class AsyncSender {
+    private static final Logger logger = LoggerFactory.getLogger(AsyncSender.class);
+
+    public static void main(String[] args) {
+        Properties props = initProps();
+        String topic = "fire-and-forget-sender";
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        IntStream.range(0, 10).forEach(i -> {
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<>(topic, String.valueOf(i), "hello " + i);
+            Future<RecordMetadata> future = producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    logger.info("message {} send done. partition {}, offset {}", i, metadata.partition(), metadata.offset());
+                }
+            });
+
+        });
+    }
+
+    private static Properties initProps() {
+        final Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.19.30:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return props;
+    }
+}
